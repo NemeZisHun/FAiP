a0b1d70890b57972941d7403fe839f9efea9893e
NemeZisHun <bakos.mate94@gmail.com>
2018-02-04 13:17:06 +0100

    Inital commit, setting up repo

---
+++ Chapter1EditorIntro.py
@@ -0,0 +1,18 @@
+# This is the test code from page 24
+print('Welcome to your first basic as fuck python script!')
+print('What is your name, my dear?') #ask about name
+name = input()
+print('How about a nice cup of shut the fuck up, ' + name)
+print('Some useless info about your name, it contains ' + str(len(name)) + ' letters')
+print('Interesting huh?... ')
+
+
+print('Ok, how old are you?') #ask about age
+age = input()
+print('Gettin old huh? You will be an 80 y\o rotten sack in ' + str(80-int(age)) + ' years, scary right?')
+
+
+
+
+
+
